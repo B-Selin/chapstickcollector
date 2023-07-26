30cbd0bad581a25f15b732e7b64903c2bb7e85cf
B-Selin <selin@bozkurt.ca>
2023-07-25 21:28:42 -0600

    Include db of chapsticks

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
   
 urlpatterns = [
 	path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('chapsticks/', views.chapsticks_index, name='chapsticks_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
 
+
+chapsticks = [
+  {'location': 'Office Desk', 'flavor': 'cherry', 'description': 'my daily chapstick'},
+  {'location': 'Kitchen Counter', 'flavor': 'vanilla', 'description': 'use this one while baking'},
+  {'location': 'Car Cup Holder', 'flavor': 'mint', 'description': 'keep this one in the car'},
+  {'location': 'Left Nightstand', 'flavor': 'coconut', 'description': 'use this one before bed'},
+  {'location': 'Bathroom', 'flavor': 'coconut', 'description': 'use this one while getting ready'}
+]
+
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def chapsticks_index(request):
+    return render(request, 'chapsticks/index.html', {
+        'chapsticks': chapsticks
+    })
+
+# def chapstick_detail(request, chapstick_id):
+#     return render(request, 'chapsticks/detail.html')    
+
+# def chapstick_create(request):
+#     if request.method == 'POST':
+#         # create chapstick from form data
+#         pass
+#     else:
+#         # show form
+#         pass
+
+# def chapstick_update(request, chapstick_id):
+#     pass
+
+# def chapstick_delete(request, chapstick_id):
+#     pass
