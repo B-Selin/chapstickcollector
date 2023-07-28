5ea1d784150f43e4a4010c85764339a10bdf50f9
B-Selin <selin@bozkurt.ca>
2023-07-27 19:11:15 -0600

    include ChewMarks model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Chapstick
+from .models import Chapstick, ChewMark
 
 # Register your models here
 admin.site.register(Chapstick)
+admin.site.register(ChewMark)
--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+SIZE = (
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large')
+)
 
 class Chapstick(models.Model):
   location = models.CharField(max_length=100)
@@ -17,3 +22,21 @@ class Chapstick(models.Model):
   
   def get_absolute_url(self):
       return reverse('detail', kwargs={'chapstick_id': self.id}) 
+  
+
+# make a new model here called ChewMarks
+
+class ChewMark(models.Model):
+  date = models.DateField()
+  size = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=SIZE,
+    # set the default value for bite to be 'S'
+    default=SIZE[0][0]
+   )
+  chapstick = models.ForeignKey(Chapstick, on_delete=models.CASCADE)
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_size_display()} on {self.date}"
